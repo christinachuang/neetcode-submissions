2ed265d98f13a93699822958d6e632a42a316341
christinachuang <c10032327@gmail.com>
2026-07-27 18:24:51 +0800

    Add: longest-increasing-subsequence - submission-4

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-4.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.empty())
+            return 0;
+        vector<int> lis;
+        lis.push_back(nums[0]);
+        for(int i = 1; i < nums.size(); i++){
+            binarySearch(nums[i], lis);
+        }
+        return lis.size();
+    }
+    void binarySearch(int curr, vector<int>& lis){
+        int left = 0, right = lis.size() - 1;
+        while(left <= right && curr != lis[left]){
+            int mid = left + (right - left)/2;
+            if(curr < lis[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;   
+        }
+        if(left < lis.size())
+            lis[left] = curr;
+        else
+            lis.push_back(curr);
+    }
+};
