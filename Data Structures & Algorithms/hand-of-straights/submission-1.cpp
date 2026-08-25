c2b603bf2078c9c7768591ae2ab79642ca84a8c2
christinachuang <c10032327@gmail.com>
2026-08-25 16:29:39 +0800

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int size = hand.size();
+        if(size % groupSize != 0)
+            return false;
+        int groups = size / groupSize;
+        
+        map<int, int> lookup;
+        for(const auto& x : hand)
+            lookup[x]++;
+        for(int round = 0; round < groups; round++){
+            int start_val = lookup.begin()->first;
+            for(int curr = 0; curr < groupSize; curr++){
+                int target = start_val + curr;
+                auto find = lookup.find(target);
+                if(find == lookup.end() || find->second == 0)
+                    return false;
+                find->second--;
+                if(find->second == 0)
+                    lookup.erase(target);
+            }
+        }
+        return true;
+    }
+};
